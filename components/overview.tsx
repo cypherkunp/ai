541408307e5cde6cae98fdbbacff6591563a46c5
Devvrat <devvratshukla@gmail.com>
2024-11-28 02:30:42 +0000

    feat: tony's intro

--- components/overview.tsx
+++ components/overview.tsx
@@ -1,7 +1,8 @@
-import { motion } from 'framer-motion';
-import Link from 'next/link';
+import { motion } from "framer-motion";
+import Link from "next/link";
 
-import { MessageIcon, VercelIcon } from './icons';
+import { MessageIcon, VercelIcon } from "./icons";
+import { Badge } from "./ui/badge";
 
 export const Overview = () => {
   return (
@@ -13,39 +14,12 @@ export const Overview = () => {
       exit={{ opacity: 0, scale: 0.98 }}
       transition={{ delay: 0.5 }}
     >
-      <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
-        <p className="flex flex-row justify-center gap-4 items-center">
-          <VercelIcon size={32} />
-          <span>+</span>
-          <MessageIcon size={32} />
-        </p>
-        <p>
-          This is an{' '}
-          <Link
-            className="font-medium underline underline-offset-4"
-            href="https://github.com/vercel/ai-chatbot"
-            target="_blank"
-          >
-            open source
-          </Link>{' '}
-          chatbot template built with Next.js and the AI SDK by Vercel. It uses
-          the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">streamText</code>{' '}
-          function in the server and the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">useChat</code> hook
-          on the client to create a seamless chat experience.
-        </p>
-        <p>
-          You can learn more about the AI SDK by visiting the{' '}
-          <Link
-            className="font-medium underline underline-offset-4"
-            href="https://sdk.vercel.ai/docs"
-            target="_blank"
-          >
-            docs
-          </Link>
-          .
-        </p>
+      <div className="rounded-xl p-6 flex flex-col gap-2 leading-relaxed text-left max-w-xl">
+        <h1 className="text-5xl sm:text-7xl font-bold tracking-tighter">
+          Hey, I am Tony
+        </h1>
+
+        <p className="text-lg font-light">How can I help you today?</p>
       </div>
     </motion.div>
   );
